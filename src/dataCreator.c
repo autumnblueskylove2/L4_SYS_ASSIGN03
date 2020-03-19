7dd32bc8ce2d6c951a2ad00df7fc196643316636
autumnblueskylove <iamfavored@hanmail.net>
2020-03-19 02:05:59 -0400

    int version

---
+++ src/dataCreator.c
@@ -0,0 +1,22 @@
+﻿/*
+ * File        : dataCreator.c
+ * Project     : Hoochmacallit
+ * By          : Hyungbum Kim and Charng Gwon Lee
+ * Date        : March 21, 2020
+ * Description : This program is to generate an S19 download file format or an
+ *               assembly file which are both readable by human.
+ *               This utility takes any binary input file and fransforms it
+ *               into S-Record output file, or an assembly file for use in an
+ *               embedded software development environment.
+ */
+
+#include "../inc/dataCreator.h"
+
+int main()
+{
+    int i = 2;
+    
+    printf("dataCreator: %d\n", i);
+    
+    return (0);
+}
